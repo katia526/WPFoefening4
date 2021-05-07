099c54c8fce1a3b2087afa9f4923642dd72a0355
katia verachtert <katia.verachtert.scherpendries18@outlook.com>
2021-05-07 16:01:19 +0200

    Add project files.

---
+++ WPFoefening4/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFoefening4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            var tot = Convert.ToInt32(txt1.Text) + Convert.ToInt32(txt2.Text);
+            totaal.Text = tot.ToString();
+        }
+
+        private void Min_Click(object sender, RoutedEventArgs e)
+        {
+            var tot = Convert.ToInt32(txt1.Text) - Convert.ToInt32(txt2.Text);
+            totaal.Text = tot.ToString();
+        }
+
+        private void Maal_Click(object sender, RoutedEventArgs e)
+        {
+            var tot = Convert.ToInt32(txt1.Text) * Convert.ToInt32(txt2.Text);
+            totaal.Text = tot.ToString();
+        }
+
+        private void Deel_Click(object sender, RoutedEventArgs e)
+        {
+            var tot = Convert.ToInt32(txt1.Text) / Convert.ToInt32(txt2.Text);
+            totaal.Text = tot.ToString();
+        }
+    }
+}
